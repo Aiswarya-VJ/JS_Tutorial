50ecb705c813596330ca03416703ed478877cd15
Aiswarya V J <aiswaryaanandhu9@gmail.com>
2024-09-30 02:20:45 +0530

    Initial commit

---
+++ sample.js
@@ -0,0 +1,113 @@
+document.write("Learn javascript");//text displaying
+
+//alert("hello world")//showing a pop up alert
+
+console.log("hai")//output visible only at inspect code space 
+
+document.write("<h1>Learn JavaScript</h1>")//size variation of text display
+
+
+var aiswarya;//variable declaration
+var a = "aiswarya";//variable assign
+document.write(a);
+document.write("<br>");
+document.write(typeof a);
+document.write("<br>");
+
+//variable creation
+var n1 = 10;//number storage
+var n2 = 10.45;
+document.write("<br>");
+document.write(n1);
+document.write("<br>");
+document.write(typeof n1);
+document.write("<br>");
+document.write(n2);
+document.write("<br>");
+document.write(typeof n1);
+document.write("<br>");
+var js = true;//boolean datatype
+var html = false;
+document.write("<br>");
+document.write(js);
+document.write("<br>");
+document.write(typeof js);
+document.write("<br>");
+
+document.write("<br>");
+document.write(html);
+document.write("<br>");
+document.write(typeof html);
+document.write("<br>");
+var test;//undefined(no value assigned)
+var test2 = null;//assigning null value
+document.write("<br>");
+document.write(test);
+document.write("<br>");
+document.write(typeof test);
+document.write("<br>");
+
+document.write("<br>");
+document.write(test2);
+document.write("<br>");
+document.write(typeof test2);
+document.write("<br>");
+
+//string with break
+var a = "aiswarya";//variable assign
+document.write("<br>" + a + "<br>");
+
+var d = 10, c = 20;//number assign and add the numbers
+document.write("<br>" + (d + c) + "<br>");
+
+//constant assign
+const pi = 3.14
+document.write("<br>" + pi);
+
+//operators
+var x = 20;
+var y = 10;
+document.write("<br><br>" + (x + y));//addition
+document.write("<br>" + (x - y));//sub
+document.write("<br>" + (x * y));//multi
+document.write("<br>" + (x / y));//division
+document.write("<br>" + (x % y));//mod
+document.write("<br>");
+x++;//increment
+document.write(x);
+document.write("<br>");
+y--;//decrement
+document.write(y);
+document.write("<br>");
+document.write(2 ** 2);//square
+document.write("<br>");
+document.write(2 ** 3);//cube
+
+//assignment operators
+var s = 5;//assign
+document.write("<br>");
+document.write(s);
+var result = s += 4//add and assign
+document.write("<br>");
+document.write(result);
+var result = s *= 4//multiply and assign
+document.write("<br>");
+document.write(result);
+var result = s %= 4//mod and assign
+document.write("<br>");
+document.write(result);
+
+//comparison operators
+document.write("<br>");
+document.write(" comparison operators");
+var p = 20;
+var q = 10;
+document.write("<br>");
+document.write(p === q); //same or not value and also datatypes
+document.write("<br>");
+document.write(p == q);//check only value
+document.write("<br>");
+document.write(p > q);//check greater or equals
+document.write("<br>");
+
+
